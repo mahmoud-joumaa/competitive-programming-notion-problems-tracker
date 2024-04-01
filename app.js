6378733aba7cb87d1c7192a19cf7c720293a16d5
MahmoudJLB <mahmoud.j.eschool@gmail.com>
2024-04-01 15:23:54 +0300

    Cleaned leetcode graphql query

--- app.js
+++ app.js
@@ -17,69 +17,13 @@ async function main() {
 	const leetcode_query_get_problem = `
 		query ($titleSlug: String!) {
 			question(titleSlug: $titleSlug) {
-				questionId
-				questionFrontendId
-				boundTopicId
-				title
-				titleSlug
-				content
-				translatedTitle
-				translatedContent
-				isPaidOnly
 				difficulty
-				likes
-				dislikes
-				isLiked
-				similarQuestions
-				exampleTestcases
-				contributors {
-					username
-					profileUrl
-					avatarUrl
-				}
 				topicTags {
 					name
-					slug
-					translatedName
 				}
-				companyTagStats
-				codeSnippets {
-					lang
-					langSlug
-					code
-				}
-				stats
-				hints
-				solution {
-					id
-					canSeeDetail
-					paidOnly
-					hasVideoSolution
-					paidOnlyVideo
-				}
-				status
-				sampleTestCase
-				metaData
-				judgerAvailable
-				judgeType
-				mysqlSchemas
-				enableRunCode
-				enableTestMode
-				enableDebugger
-				envInfo
-				libraryUrl
-				adminUrl
-				challengeQuestion {
-					id
-					date
-					incompleteChallengeCount
-					streakCount
-					type
-				}
-				note
 			}
 		}
-	`; // TODO: Clean query
+	`;
 
 	const vjudge_name = "vjudge";
 	const vjudge_max_results_length = 20; // the max that can be retrieved per request
