d6efb62397a1818ebe3086ca8dbf0068f9e11652
MahmoudJLB <mahmoud.j.eschool@gmail.com>
2024-04-06 14:30:57 +0300

    Adding proxies again

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ async function main() {
 
 	const { Client } = require('@notionhq/client');
 
-	// global variables
-	let requests = 0; // track number of requests
-
 	// platform-specific properties
 	const codeforces_name = "codeforces";
 	const codeforces_id_threshold = 100000; // any id greater than this is placed in the gym
@@ -112,10 +109,10 @@ async function main() {
 			switch (platform.name) {
 				case codeforces_name:
 					await sleep(sleep_duration.codeforces_name);
-					return cheerio.load((await axios.get(this.url)).data)("#program-source-text").text();
+					return cheerio.load((await axios.get(this.url, {proxy: await getRandomProxy()})).data)("#program-source-text").text();
 				case vjudge_name:
 					await sleep(sleep_duration.vjudge_name);
-					return (await axios.get(`https://vjudge.net/solution/data/${this.id}`)).data.codeAccessInfo; // FIXME: Take into account the actual text while logged in
+					return (await axios.get(`https://vjudge.net/solution/data/${this.id}`, {proxy: await getRandomProxy()})).data.codeAccessInfo; // FIXME: Take into account the actual text while logged in
 					case leetcode_name:
 						return this.code;
 			}
@@ -191,6 +188,7 @@ async function main() {
 		static async getCSRF(platform_name) {
 			const baseUrl = Platform.generateBaseUrl(platform_name)
 			const cookies_raw = await axios.get(baseUrl, {
+				proxy: await getRandomProxy(),
 				headers: {
 					"content-type": "application/json",
 					origin: baseUrl,
@@ -251,14 +249,14 @@ async function main() {
 				case codeforces_name:
 					const randomSig = Platform.generateRandomString(6);
 					const fetchRequestParams = `user.status?apiKey=${process.env.CODEFORCES_KEY}&handle=${process.env.CODEFORCES_ID}&time=${now}`;
-					submissions = await axios.get(`${this.base_url}/${fetchRequestParams}&apiSig=${randomSig}${crypto.createHash('sha512').update(`${randomSig}/${fetchRequestParams}#${process.env.CODEFORCES_SECRET}`).digest('hex')}`);
+					submissions = await axios.get(`${this.base_url}/${fetchRequestParams}&apiSig=${randomSig}${crypto.createHash('sha512').update(`${randomSig}/${fetchRequestParams}#${process.env.CODEFORCES_SECRET}`).digest('hex')}`, {proxy: await getRandomProxy()});
 					return (submissions.data.result).filter(submission => submission.creationTimeSeconds > this.last_submission_timestamp);
 				case vjudge_name:
 					submissions = [];
 					let i = 0;
 					while (true) {
 						await sleep(sleep_duration.vjudge_name);
-						let currentSubmissions = await axios.get(`${this.base_url}/status/data?start=${i*vjudge_max_results_length}&length=${vjudge_max_results_length}&un=${process.env.VJUDGE_ID}`);
+						let currentSubmissions = await axios.get(`${this.base_url}/status/data?start=${i*vjudge_max_results_length}&length=${vjudge_max_results_length}&un=${process.env.VJUDGE_ID}`, {proxy: await getRandomProxy()});
 						if (currentSubmissions.data.data.length == 0) break;
 						submissions.push(...currentSubmissions.data.data);
 						i++;
@@ -294,7 +292,7 @@ async function main() {
 				case vjudge_name:
 					id = `${data.oj}/${data.probNum}`;
 					await sleep(sleep_duration.vjudge_name);
-					name = await axios.get(`${this.base_url}/problem/data?start=0&length=1&OJId=${data.oj}&probNum=${data.probNum}&title=&source=&category=`);
+					name = await axios.get(`${this.base_url}/problem/data?start=0&length=1&OJId=${data.oj}&probNum=${data.probNum}&title=&source=&category=`, {proxy: await getRandomProxy()});
 					name = name.data.data[0].title;
 					break;
 				case leetcode_name:
@@ -302,6 +300,7 @@ async function main() {
 					name = data.title;
 					await sleep(sleep_duration.leetcode_name);
 					const problem_info = await axios.post(`${this.base_url}/graphql`, {
+						proxy: await getRandomProxy(),
 						headers: Platform.generateLeetcodeHeaders(Platform.getCSRF(leetcode_name)),
 						query: leetcode_query_get_problem,
 						variables: { "titleSlug": data.title_slug }
@@ -603,13 +602,22 @@ async function main() {
 		return Math.floor(Math.random()*max);
 	}
 
+	async function getRandomProxy(protocol="https") {
+		const proxies = (await axios.get(`https://raw.githubusercontent.com/zloi-user/hideip.me/main/${protocol}.txt`)).data.split('\n');
+		for (let i = 0; i < proxies.length; i++) proxies[i] = proxies[i].substring(0, proxies[i].lastIndexOf(':'));
+		const proxy =  proxies[generateRandomNumber(proxies.length)];
+		return {
+			protocol: protocol,
+			host: proxy.split(':')[0],
+			port: Number(proxy.split(':')[1])
+		};
+	}
+
 	async function randomWait() {
-		if (requests%10 == 0) return new Promise(resolve => setTimeout(resolve, 6000));
 		return new Promise(resolve => setTimeout(resolve, generateRandomNumber(1001)));
 	}
 
 	async function sleep(duration) {
-		requests++;
 		randomWait();
 		return new Promise(resolve => setTimeout(resolve, duration));
 	}
@@ -629,6 +637,9 @@ async function main() {
 	// ==============================================================================================
 
 	try {
+
+		// for (let i = 0; i < 1000; i++) console.log(await getRandomProxy()); exitApplication(2); // TEMP:
+		for (let i = 0; i < 1000; i++) {const proxy = await getRandomProxy("https"); console.log(proxy); console.log((await axios.get("https://codeforces.com", {proxy: proxy})).status);} exitApplication(3); // TEMP:
 
 		// Welcoming the user
 		console.log("\n\n====================================================================================================\nWelcome to the Competitve Programming Problems Tracker in Notion\n====================================================================================================\n");
